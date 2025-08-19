b86cd355a1f831e71f8e81edfbb8703195069d2a
Silas Teague <silasteague@gmail.com>
2025-08-19 11:37:57 -0400

    Attempt activate backend from page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,18 @@
 import ChessBoard from '@/app/components/ChessBoard';
 
 export default function Page() {
+    async function activateBackend() {
+      try {
+        const response = await fetch("https://dahlia-web-irbt.onrender.com");
+        if (response) {
+          console.log("Render already activated.");
+        }
+      } catch (error) {
+        console.log("Request sent to backend. Activating backend.")
+      }
+    }
+  
+    activateBackend();
   return (
     <ChessBoard />
   );
